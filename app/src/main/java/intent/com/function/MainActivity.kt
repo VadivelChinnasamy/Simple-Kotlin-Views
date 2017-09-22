c0a9725895f120a56f4dd1ac9e99bdc00e693e62
Anoop <anoop@2basetechnologies.com>
2017-09-22 08:53:43 +0530

    Kotlin first commit

---
+++ app/src/main/java/intent/com/function/MainActivity.kt
@@ -0,0 +1,55 @@
+package intent.com.function
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*****************TEXTVIEW************************/
+        textview.text = "Kotlin textview example"
+        textview.setTextColor(Color.RED)
+        textview.maxLines = 10
+
+        textview.setOnClickListener {
+            Toast.makeText(this, "=>" + start(), Toast.LENGTH_LONG).show()
+
+        }
+        /*********************BUTTON********************/
+        button.text = "Button On Click"
+        button.setBackgroundColor(Color.CYAN);
+        button.background = ContextCompat.getDrawable(this, R.drawable.ic_launcher_background)
+
+        /*********************BUTTON*ONCLICK*******************/
+        button.setOnClickListener {
+            view: View? ->
+            if (view != null) {
+                var x = view.id
+                Toast.makeText(this, "Onclick" + x, Toast.LENGTH_LONG).show()
+            }
+            setValues()
+
+        }
+        /************************************************/
+
+
+    }
+
+    /***********************FUCNTION CALLING*****************************/
+    fun setValues() { this.textview.text = "Welcome to Kotlin" }
+
+    fun start(): String = "HelloWorld!!!"
+
+}
